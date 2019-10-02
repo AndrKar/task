1d8dde7c581824c98f71f5c727d6a1da8e5b3ee5
Andrii KARPUS <andriikarpus@gmail.com>
2019-10-02 22:33:19 +0200

    add two for cycles: a_list will prin even numbers, b_list will print odd numbers

--- file.py
+++ file.py
@@ -1,2 +1,11 @@
 a_list = [i for i in range(25)]
 b_list = [i ** 2 for i in range(25)]
+
+for _ in a_list:
+    if _ % 2 == 0:
+        print(_)
+
+
+for _ in b_list:
+    if _ % 2 != 0:
+        print(_)
